3ad9ef93a66bee49b66639a8e19f0025ad3e52c2
Shadow-Striker <82645925+Shadow-Striker@users.noreply.github.com>
2021-11-05 20:20:02 +0000

    Players now collide correctly

--- GrenadeBattle/main.cpp
+++ GrenadeBattle/main.cpp
@@ -1,6 +1,5 @@
 #include <SFML/Graphics.hpp>
 #include <Math.h>
-#include <Player.h>
 #include <SFML/Audio.hpp>
 #include <iostream>
 #include <string>
@@ -10,6 +9,7 @@ void PlayerTwoInput(sf::Vector2f & _playerTwoAcceleration, float _ACCEL_RATE, fl
 void PlayerMovement(sf::Vector2f & _playerVelocity, sf::Vector2f & _playerAcceleration, float _deltaTime, sf::Vector2f & _playerPosition, float _drag);
 bool IntersectCheck(sf::Vector2f _spherePos, sf::Vector2f _otherPos, float _radius);
 void Intersect(sf::FloatRect _playerRect, sf::FloatRect _playerTwoRect, sf::Vector2f _playerCenter, sf::Vector2f _playerTwoCenter, sf::Vector2f _colDepth, sf::Vector2f _playerPos, sf::Vector2f _playerVelocity);
+void IntersectTwo(sf::FloatRect & _playerRect, sf::FloatRect & _playerTwoRect, sf::Vector2f & _playerCenter, sf::Vector2f & _playerTwoCenter, sf::Vector2f & _colDepth, sf::Vector2f & _playerPos, sf::Vector2f & _playerVelocity, sf::RectangleShape & _playerRectDisplay, sf::RectangleShape & _playerTwoRectDisplay);
 
 int main()
 {
@@ -17,7 +17,6 @@ int main()
 	sf::Texture playerTexture;
 	playerTexture.loadFromFile("Assets/Sprites/boy.png");
 
-/* Player Code
 	//---- PLAYER ONE CODE ----
 	sf::Sprite playerSprite;
 	playerSprite.setTexture(playerTexture);
@@ -27,22 +26,33 @@ int main()
 	
 	//Velocity
 	sf::Vector2f playerVelocity = sf::Vector2f(200.0f, 0.0f);
-	sf::Vector2f terminalVelocity = sf::Vector2f(0.0f, 15.0f);
+	sf::Vector2f terminalVelocity = sf::Vector2f(0.0f, 20.0f);
 	
 	//Gravity times 2 is being applied in playerAcceleration.
 	sf::Vector2f playerAcceleration = sf::Vector2f(0.0f, 0.0f);
 
-	//Rects
-	sf::FloatRect playerRect = playerSprite.getGlobalBounds();
-	sf::Vector2f playerRectSize = sf::Vector2f(playerRect.width, playerRect.height);
-
 	//Bounding boxes
-	sf::Vector2f playerBoundsOffset(3.0f, 0.0f);
-	sf::Vector2f playerBoundsScale(0.75f, 1.0f);
+	sf::Vector2f playerBoundsOffset(0.0f, 0.0f);
+	sf::Vector2f playerBoundsScale(1.0f, 1.0f);
+
+	sf::Vector2f spriteSize;
+	spriteSize.x = playerTexture.getSize().x;
+	spriteSize.y = playerTexture.getSize().y;
+
+	// Bounding circles
+	/*float playerCircleRadius = playerTexture.getSize().x / 2;
+	sf::Vector2f playerCircleCentre = playerPosition + 0.5f * spriteSize;
+	sf::CircleShape playerCircle;
+	playerCircle.setRadius(playerCircleRadius);
+	playerCircle.setPosition(playerPosition);
+	playerCircle.setFillColor(sf::Color::Green);*/
+
+	//Visual
+	sf::RectangleShape playerRectDisplay;
 
 	//Center of player
 		//Setup player center
-	sf::Vector2f playerCenter = sf::Vector2f(0.0f, 0.0f);
+	sf::Vector2f playerCenter = playerPosition + 0.5f * spriteSize;
 
 
 	//---- END OF PLAYER ONE CODE ----
@@ -59,23 +69,28 @@ int main()
 
 	//Gravity times 2 is being applied in playerAcceleration.
 	sf::Vector2f playerTwoAcceleration = sf::Vector2f(0.0f, 0.0f);
-
-	//Rect
-	sf::FloatRect playerTwoRect = playerSprite.getGlobalBounds();
-	sf::Vector2f playerTwoRectSize = sf::Vector2f(playerTwoRect.width, playerTwoRect.height);
 	
 	//Bounding Boxes
-	sf::Vector2f playerTwoBoundsOffset(-7.0f, -7.0f);
-	sf::Vector2f playerTwoBoundsScale(1.5f, 1.5f);
+	sf::Vector2f playerTwoBoundsOffset(0.0f, 0.0f);
+	sf::Vector2f playerTwoBoundsScale(1.0f, 1.0f);
+
+	
+	//Bounding Circle
+	/*float playerTwoCircleRadius = playerTexture.getSize().x / 2;
+	sf::Vector2f playerTwoCircleCentre = playerTwoPosition + 0.5f * spriteSize;
+	sf::CircleShape playerTwoCircle;
+	playerTwoCircle.setRadius(playerTwoCircleRadius);
+	playerTwoCircle.setPosition(playerTwoPosition);
+	playerTwoCircle.setFillColor(sf::Color::Green);*/
+
+	//Visual
+	sf::RectangleShape playerTwoRectDisplay;
 
 	//Center of player two
 	//Setup player center
-	sf::Vector2f playerTwoCenter = sf::Vector2f(0.0f, 0.0f);
+	sf::Vector2f playerTwoCenter = playerTwoPosition + 0.5f * spriteSize;
 
 	//---- END OF PLAYER TWO CODE ----
-*/
-
-	Player playerOne();
 
 	float jumpForce = 100.0f;
 	float playerSpeed = 3000.0f;
@@ -103,10 +118,10 @@ int main()
 		}
 
 		playerAcceleration.x = 0.0f;
-		playerAcceleration.y = 15.0f;
+		playerAcceleration.y = 20.0f;
 
 		playerTwoAcceleration.x = 0.0f;
-		playerTwoAcceleration.y = 15.0f;
+		playerTwoAcceleration.y = 20.0f;
 
 		PlayerOneInput(playerAcceleration, ACCEL_RATE, jumpForce);
 		PlayerTwoInput(playerTwoAcceleration, ACCEL_RATE, jumpForce);
@@ -135,15 +150,58 @@ int main()
 		playerSprite.setPosition(playerPosition);
 		playerTwoSprite.setPosition(playerTwoPosition);
 
+		playerCenter = playerPosition + 0.5f * spriteSize;
+		playerTwoCenter = playerTwoPosition + 0.5f * spriteSize;
+
+		//Rects
+		sf::FloatRect playerRect = playerSprite.getGlobalBounds();
+		sf::Vector2f playerRectSize = sf::Vector2f(playerRect.width, playerRect.height);
+
+		sf::FloatRect playerTwoRect = playerTwoSprite.getGlobalBounds();
+		sf::Vector2f playerTwoRectSize = sf::Vector2f(playerTwoRect.width, playerTwoRect.height);
+
+		//Geometry
+
+		playerRect.left += playerBoundsOffset.x;
+		playerRect.top += playerBoundsOffset.y;
+		playerRect.width *= playerBoundsScale.x;
+		playerRect.height *= playerBoundsScale.y;
+
+		playerTwoRect.left += playerTwoBoundsOffset.x;
+		playerTwoRect.top += playerTwoBoundsOffset.y;
+		playerTwoRect.width *= playerTwoBoundsScale.x;
+		playerTwoRect.height *= playerTwoBoundsScale.y;
+
+		//Update visuals
+		//playerCircle.setPosition(playerPosition);
+
+		playerRectDisplay.setPosition(playerRect.left, playerRect.top);
+		playerTwoRectDisplay.setPosition(playerTwoRect.left, playerTwoRect.top);
+
+		playerRectDisplay.setSize(sf::Vector2f(playerRect.width, playerRect.height));
+		playerTwoRectDisplay.setSize(sf::Vector2f(playerTwoRect.width, playerTwoRect.height));
+
 		if (playerRect.intersects(playerTwoRect))
 		{
-			Intersect(playerRect, playerTwoRect, playerCenter, playerTwoCenter, colDepth, playerPosition, playerVelocity);
+			IntersectTwo(playerRect, playerTwoRect, playerCenter, playerTwoCenter, colDepth, playerPosition, playerVelocity, playerRectDisplay, playerTwoRectDisplay);
+			playerSprite.setPosition(playerPosition);
+			playerTwoSprite.setPosition(playerTwoPosition);
+		}
+		else 
+		{
+			sf::Color fillcolor = sf::Color::Green;
+			fillcolor.a = 125;
+
+			playerRectDisplay.setFillColor(fillcolor);
+			playerTwoRectDisplay.setFillColor(fillcolor);
 		}
 
 
 		window.clear();
 		window.draw(playerSprite);
 		window.draw(playerTwoSprite);
+		window.draw(playerRectDisplay);
+		window.draw(playerTwoRectDisplay);
 		window.display();
 	}
 
@@ -238,4 +296,52 @@ void Intersect(sf::FloatRect _playerRect, sf::FloatRect _playerTwoRect, sf::Vect
 		_playerVelocity.x = 0;
 	}
 	_playerVelocity = sf::Vector2f(0.0f, 0.0f);
+}
+
+void IntersectTwo(sf::FloatRect & _playerRect, sf::FloatRect & _playerTwoRect, sf::Vector2f & _playerCenter, sf::Vector2f & _playerTwoCenter, sf::Vector2f & _colDepth, sf::Vector2f & _playerPos, sf::Vector2f & _playerVelocity, sf::RectangleShape & _playerRectDisplay, sf::RectangleShape & _playerTwoRectDisplay)
+{
+	sf::Color fillcolor = sf::Color::Red;
+	fillcolor.a = 125;
+
+	//playerCircle.setFillColor(fillcolor);
+	//enemyCircle.setFillColor(fillcolor);
+
+	_playerRectDisplay.setFillColor(fillcolor);
+	_playerTwoRectDisplay.setFillColor(fillcolor);
+	
+	// Collision Depth
+	sf::Vector2f AWidthHeight = sf::Vector2f(_playerRect.width, _playerRect.height);
+	sf::Vector2f BWidthHeight = sf::Vector2f(_playerTwoRect.width, _playerTwoRect.height);
+
+	sf::Vector2f ACentre = sf::Vector2f(_playerRect.left, _playerRect.top);
+	ACentre += 0.5f * AWidthHeight;
+	sf::Vector2f BCentre = sf::Vector2f(_playerTwoRect.left, _playerTwoRect.top);
+	BCentre += 0.5f * BWidthHeight;
+
+	sf::Vector2f distance = BCentre - ACentre;
+	sf::Vector2f minDistance = 0.5f * AWidthHeight + 0.5f * BWidthHeight;
+	if (distance.x < 0)
+		minDistance.x = -minDistance.x;
+	if (distance.y < 0)
+		minDistance.y = -minDistance.y;
+
+	sf::Vector2f depth = minDistance - distance;
+	sf::Vector2f absDepth = sf::Vector2f(abs(depth.x), abs(depth.y));
+
+	if (absDepth.x < absDepth.y)
+	{
+		// Move along x direction
+		_playerPos.x -= depth.x;
+
+		// Stop movement in x direction
+		_playerVelocity.x = 0;
+	}
+	else
+	{
+		// Move along y direction
+		_playerPos.y -= depth.y;
+
+		// Stop movement in y direction
+		_playerVelocity.y = 0;
+	}
 }
